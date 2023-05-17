345e59e5168abc0599230b2bad83b1f52aa36dc7
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-05-17 22:35:37 +0530

    Added source code of car-rantal-app backend

---
+++ keys.js
@@ -0,0 +1,7 @@
+// MONGO_URI ={
+
+// }
+module.exports = {
+    MONGO_URI:"mongodb+srv://joshianuvrat:7055242152@cluster0.8inctq1.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"avbgdfr"
+}
